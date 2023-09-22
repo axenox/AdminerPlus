aeaba9a025844dcc7ead94bf3bbf716a2912691c
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2023-09-22 15:15:18 +0200

    FIX improved arrow titles in DB schema

--- adminer/schema.inc.php
+++ adminer/schema.inc.php
@@ -71,8 +71,8 @@ foreach ($schema as $name => $table) {
 		foreach ($refs as $left => $ref) {
 			$left1 = $left - $table_pos[$name][1];
 			$i = 0;
-			foreach ($ref[0] as $source) {
-				echo "\n<div class='references' title='" . h($target_name) . "' id='refs$left-" . ($i++) . "' style='left: $left1" . "em; top: " . $table["fields"][$source]["pos"] . "em; padding-top: .5em;'><div style='border-top: 1px solid Gray; width: " . (-$left1) . "em;'></div></div>";
+			foreach ($ref[0] as $iS => $source) {
+				echo "\n<div class='references' title='" . h("$name.$source -> $target_name.{$ref[1][$iS]}") . "' id='refs$left-" . ($i++) . "' style='left: $left1" . "em; top: " . $table["fields"][$source]["pos"] . "em; padding-top: .5em;'><div style='border-top: 1px solid Gray; width: " . (-$left1) . "em;'></div></div>";
 			}
 		}
 	}
@@ -82,7 +82,7 @@ foreach ($schema as $name => $table) {
 			$left1 = $left - $table_pos[$name][1];
 			$i = 0;
 			foreach ($columns as $target) {
-				echo "\n<div class='references' title='" . h($target_name) . "' id='refd$left-" . ($i++) . "' style='left: $left1" . "em; top: " . $table["fields"][$target]["pos"] . "em; height: 1.25em; background: url(../adminer/static/arrow.gif) no-repeat right center;'><div style='height: .5em; border-bottom: 1px solid Gray; width: " . (-$left1) . "em;'></div></div>";
+			    echo "\n<div class='references' title='" . h("$name.$source -> $target_name.{$ref[1][$iS]}") . "' id='refd$left-" . ($i++) . "' style='left: $left1" . "em; top: " . $table["fields"][$target]["pos"] . "em; height: 1.25em; background: url(../adminer/static/arrow.gif) no-repeat right center;'><div style='height: .5em; border-bottom: 1px solid Gray; width: " . (-$left1) . "em;'></div></div>";
 			}
 		}
 	}
